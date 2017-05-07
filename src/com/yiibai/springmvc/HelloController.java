c4b955bc10a35b78f9e6f3c4ce900023e7f092a5
nymar123 <332122152@qq.com>
2017-05-07 17:51:05 +0800

    modify

--- src/com/yiibai/springmvc/HelloController.java
+++ src/com/yiibai/springmvc/HelloController.java
@@ -44,14 +44,23 @@ public class HelloController{
    
    @RequestMapping(value="/redirect")
    public String doPublish(HttpServletRequest request) throws MqttException {
+	  //获取前台传过来的两个参数
 	  TOPIC=request.getParameter("topic");
 	  MESSAGE=request.getParameter("message");
+	  //new mqttClient
+	  //MemoryPersistence设置clientid的保存形式，默认为以内存保存
 	  client = new MqttClient(HOST, clientid, new MemoryPersistence());  
-      connect();
+      //与activeMQ连接的方法
+	  connect();
+	  //new mqttMessage
       message = new MqttMessage();  
+      //设置服务质量
       message.setQos(2);  
-      message.setRetained(true);  
+      //设置是否在服务器中保存消息体 
+      message.setRetained(true);
+      //设置消息的内容
       message.setPayload(MESSAGE.getBytes());  
+      //发布
       publish(topic, message); 
 
       System.out.println("已发送");  
@@ -60,8 +69,12 @@ public class HelloController{
    }
    
    private void connect() {  
-       MqttConnectOptions options = new MqttConnectOptions();  
+       // new mqttConnection 用来设置一些连接的属性
+	   MqttConnectOptions options = new MqttConnectOptions();  
+	   // 设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录，这里设置为true表示每次连接到服务器都以新的身份连接 
+	   // 换而言之，设置为false时可以客户端可以接受离线消息
        options.setCleanSession(false);  
+       // 设置连接的用户名和密码
        options.setUserName(userName);  
        options.setPassword(passWord.toCharArray());  
        // 设置超时时间  
@@ -69,8 +82,11 @@ public class HelloController{
        // 设置会话心跳时间  
        options.setKeepAliveInterval(20);  
        try {  
+    	   // 设置回调类
            client.setCallback(new PushCallback());  
-           client.connect(options);  
+           // 连接
+           client.connect(options);
+           // 获取activeMQ上名为TOPIC的topic
            topic = client.getTopic(TOPIC);  
        } catch (Exception e) {  
            e.printStackTrace();  
@@ -78,7 +94,10 @@ public class HelloController{
    }  
  
    public void publish(MqttTopic topic, MqttMessage message) throws MqttPersistenceException,  MqttException {  
-       MqttDeliveryToken token = topic.publish(message);  
+	   // 发布的方法
+	   // new mqttDeliveryToken
+       MqttDeliveryToken token = topic.publish(message);
+       // 发布
        token.waitForCompletion();  
        System.out.println("message is published completely! "  
                + token.isComplete());  
